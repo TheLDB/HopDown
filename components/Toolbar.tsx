7be3434247be56f9f019dfa5217830ea3d42c84e
TheLDB <ldb@erikboles.com>
2022-09-02 11:13:25 -0600

    feat(tooling): Added lower menu bar and top right icons for tools

---
+++ components/Toolbar.tsx
@@ -0,0 +1,7 @@
+const Toolbar = () => {
+    return (
+            <div className="absolute w-1/2 h-32 bottom-0 rounded-t-2xl drop-shadow-2xl bg-white left-0 right-0 m-auto"></div>
+    )
+}
+
+export default Toolbar;
